fbf84475df2811d7fb9b7967d75bf4dcb9e43849
Flame21i <110921004+Flame21i@users.noreply.github.com>
2026-08-30 06:59:40 +0300

    Update vite.config.js
    vercel debug3

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { nodePolyfills } from 'vite-plugin-node-polyfills';
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 export default defineConfig({
-  plugins: [tailwindcss(), react(),nodePolyfills()],
+  plugins: [
+    tailwindcss(), 
+    react(), 
+    nodePolyfills()
+  ],
   build: {
+    // Standard Rollup options (Vite v5/v6)
     rollupOptions: {
+      external: [
+        /^@tauri-apps\/.*/
+      ]
+    },
+    // Rolldown options (Vite v8)
+    rolldownOptions: {
       external: [
         /^@tauri-apps\/.*/
       ]
