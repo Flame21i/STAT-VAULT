868a22bd0a3a6cf526925458f2836e0bfac0f97c
Flame21i <110921004+Flame21i@users.noreply.github.com>
2026-08-30 07:08:11 +0300

    Update vite.config.js
    vercel debug 7

--- vite.config.js
+++ vite.config.js
@@ -4,17 +4,19 @@ import tailwindcss from '@tailwindcss/vite'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    react(),
-    nodePolyfills()
-  ],
+  plugins: [tailwindcss(), react(), nodePolyfills()],
+  optimizeDeps: {
+    include: ['html2canvas-pro', 'recharts', 'lucide-react'],
+  },
   build: {
+    commonjsOptions: {
+      include: [/html2canvas-pro/, /node_modules/],
+    },
     rollupOptions: {
-      external: (id) => id.startsWith('@tauri-apps/')
+      external: (id) => id.startsWith('@tauri-apps/'),
     },
     rolldownOptions: {
-      external: (id) => id.startsWith('@tauri-apps/')
-    }
-  }
+      external: (id) => id.startsWith('@tauri-apps/'),
+    },
+  },
 })
